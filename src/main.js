fa17ba2d6e9c85946692183f7529c8a187938322
akgunege1 <233301082@ogr.selcuk.edu.tr>
2025-11-22 19:36:07 +0300

    Initial Commit. First Steps.

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const cubeGeometry = new THREE.BoxGeometry(1,1,1);
+const cubeMaterial = new THREE.MeshBasicMaterial({color:'red'})
+
+//Creating Cube
+const cubeMesh = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+);
+
+//initialize camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/innerHeight, 0.1, 30);
+camera.position.z = 5;
+
+//initialize renderer
+const canvas = document.querySelector('canvas.threejs');
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+}
+);
+
+scene.add(cubeMesh);
+scene.add(camera);
+
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.render(scene, camera);
