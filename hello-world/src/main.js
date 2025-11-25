4c7d10825b5055e67772dc5f4295b30e8f5caba3
akgunege1 <233301082@ogr.selcuk.edu.tr>
2025-11-25 20:15:31 +0300

    Comment lines, antialias, eventlistener and size adjusters for better experience have been implemented

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -22,22 +22,30 @@ camera.position.z = 5;
 //initialize renderer
 const canvas = document.querySelector('canvas.threejs');
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  antialias: true  //Setting the antialias true fixes the issue.
 }
 );
 
 renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));  //If your devicePixelRatio is greater than 1, this helps aswell.
 
 //instantiate the controls
 const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-controls.autoRotate = true;
+controls.enableDamping = true; //Softens the movement of camera.
+controls.autoRotate = true; //Automaticly rotates our cube
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth/innerHeight; //Aspect ratio parameter.
+  camera.updateProjectionMatrix;  //If you are updating any kind of Parameter, You call this function so it can rearrange the props size.
+  renderer.setSize(window.innerWidth,window.innerHeight);
+})
 
 //renderloop
 const renderloop = () => {
-  controls.update();
+  controls.update(); //It updates the movement every frame so you can move the camera.
   renderer.render(scene, camera);
-  window.requestAnimationFrame(renderloop);
+  window.requestAnimationFrame(renderloop); //The renderloop itself
 };
 
 renderloop();
