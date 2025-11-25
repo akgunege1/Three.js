49f30b72497c2f62c7c97db130af687ede401628
akgunege1 <233301082@ogr.selcuk.edu.tr>
2025-11-25 21:20:05 +0300

    Advancing code with configuring object position/scale etc. Pt1

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -11,13 +11,20 @@ const cubeMesh = new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
 );
+const cubePosition = new THREE.Vector3(0,1,0); //I have declared the position for Cube.
+cubeMesh.position.copy(cubePosition); //I have set the position for the so called Cube.
+cubeMesh.scale.set(2,2,1); //You can scale the Cube via this line.
+const AxesHelper = new THREE.AxesHelper(2); //With AxesHelper you can figure the which axes you'd like to work on out.
 
+scene.add(AxesHelper); 
 scene.add(cubeMesh);
 
 //initialize Perspective Camera
 
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth/innerHeight, 0.1, 100);
 camera.position.z = 5;
+
+// console.log(cubeMesh.position.distanceTo(camera.position));  You can calculate the distance between to coordinates.
 
 //initialize renderer
 const canvas = document.querySelector('canvas.threejs');
@@ -33,7 +40,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));  //If your devicePi
 //instantiate the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true; //Softens the movement of camera.
-controls.autoRotate = true; //Automaticly rotates our cube
+controls.autoRotate = false; //Automaticly rotates our cube
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth/innerHeight; //Aspect ratio parameter.
